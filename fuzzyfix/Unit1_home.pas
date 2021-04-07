035a2ceca71c15da90ac86f058705a3bfdc623a9
Faza Muhammad Raihan <49541090+Faza-OwO@users.noreply.github.com>
2021-04-08 05:57:56 +0700

    Add files via upload

---
+++ fuzzyfix/Unit1_home.pas
@@ -0,0 +1,35 @@
+unit Unit1_home;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, jpeg, ExtCtrls;
+
+type
+  Thome = class(TForm)
+    btn_learn: TBitBtn;
+    btn_start: TBitBtn;
+    img1: TImage;
+    procedure btn_startClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  home: Thome;
+
+implementation
+  uses
+    crt,Unit2_rule,Math;
+{$R *.dfm}
+
+procedure Thome.btn_startClick(Sender: TObject);
+begin
+ruleset.Show;
+home.Hide;
+end;
+
+end.
